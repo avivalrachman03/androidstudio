19fa5f2676ed8ad9b97072ee956bc8472350d5d2
avivalrachman03 <avivalrachman03@gmail.com>
2025-06-10 15:29:07 +0800

    first commit

--- app/src/main/java/com/example/study/detail1.java
+++ app/src/main/java/com/example/study/detail1.java
@@ -9,9 +9,6 @@ import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class detail1 extends AppCompatActivity {
     Intent intent;
